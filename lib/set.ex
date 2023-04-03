84049a8094cfe4ba3a10d57497e85ac0e20571df
Tim Jarratt <tjarratt@gmail.com>
2023-04-03 19:20:36 +0200

    Step 3 : size + test refactor

--- lib/set.ex
+++ lib/set.ex
@@ -1,13 +1,17 @@
 defmodule Set do
-  defstruct empty: true
+  defstruct empty: true, size: 0
 
   def new do; %Set{} end
 
-  def new(_) do
-    %Set{empty: false}
+  def new(list) do
+    %Set{empty: false, size: length(list)}
   end
 
   def empty(set) do
     set.empty
+  end
+
+  def size(set) do
+    set.size
   end
 end
--- test/set_test.exs
+++ test/set_test.exs
@@ -2,10 +2,23 @@ defmodule SetTest do
   use ExUnit.Case
   doctest Set
 
-  test "emptiness" do
-    assert Set.empty(Set.new) === true
-    assert Set.empty(Set.new([1])) == false
-    assert Set.empty(Set.new([1, 2])) == false
+  setup_all do
+    {:ok,
+      empty: Set.new(),
+      one: Set.new([1]),
+      many: Set.new([1,2,3]),
+    }
   end
 
+  test "emptiness", state do
+    assert Set.empty(state[:empty]) === true
+    assert Set.empty(state[:one]) === false
+    assert Set.empty(state[:many]) === false
+  end
+
+  test "size", state do
+    assert Set.size(state[:empty]) === 0
+    assert Set.size(state[:one]) === 1
+    assert Set.size(state[:many]) > 1
+  end
 end
